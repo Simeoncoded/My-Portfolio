7e02ec042300adfa5fbe07ce2ed811f8590b2773
Simeoncoded <simeonolawore07@gmail.com>
2025-08-29 16:00:48 -0400

    update no icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
 //import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet],
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
