7250f8d7ebf270935a814a132c53b90c2d8feca3
Simeoncoded <simeonolawore07@gmail.com>
2025-08-29 10:58:49 -0400

    update on hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { HeroComponent } from './features/hero/hero.component';
 //import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet],
+  imports: [HeroComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
