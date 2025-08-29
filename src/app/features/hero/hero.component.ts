25d5553c7f0fd0dee34316186bb115fe7fea6cde
Simeoncoded <simeonolawore07@gmail.com>
2025-08-29 10:59:35 -0400

    update on hero component

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-hero',
     standalone: true,
-    templateUrl:'./hero-component.html',
+    templateUrl:'./hero.component.html',
     styleUrl: './hero.component.css',
 })
 export class HeroComponent{
