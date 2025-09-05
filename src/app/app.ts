559dc0d7535477fe841a63aa9e57f2aca0ab9e74
Simeoncoded <simeonolawore07@gmail.com>
2025-09-05 17:22:47 -0400

    update on contact component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
 import { AboutComponent } from './features/about/about.component';
 import { ProjectComponent } from './features/project/project.component';
+import { ContactComponent } from './features/contact/contact.component';
 //import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet],
-  imports: [HeroComponent, AboutComponent, ProjectComponent],
+  imports: [HeroComponent, AboutComponent, ProjectComponent, ContactComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
